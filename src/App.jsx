8db60d7c56da0a84734672d3ae527f96c782307d
Gokul-HP <gvarutharaj@gmail.com>
2023-11-22 00:43:18 +0530

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import './App.css'
+import { useState } from 'react'
+import Battery from './assets/NokiaBattery/Battery'
 
 function App() {
- 
+
+  const [data, setData] = useState([])
+
+  const [load, setLoad] = useState(false)
+
+  const onClick = async () => {
+    setLoad(true)
+    const url1 = "https://us-central1-my-functions-bde01.cloudfunctions.net/app/livedata";
+
+    // const url = "https://jsonplaceholder.typicode.com/users";
+    const response = await fetch(url1);
+    setData(await response.json());
+    setLoad(false)
+  }
+
   return (
-    <>
-     
-    </>
+    <div>
+      <button onClick={onClick} >get</button>
+      {load ? <div className='loader'></div> : data.map((item, i) => {
+        return (<p key={i}>{item.country}</p>)
+      })}
+
+      <Battery></Battery>
+    </div>
   )
 }
 
---
+++ src/assets/NokiaBattery/Battery.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+import "./Battery.css"
+
+const Battery = () => {
+
+    const [charge, setCharge] = useState(0)
+
+    const charging = () => {
+        setTimeout(() => {
+            if (charge <= 3) {
+                setCharge(charge + 1)
+            }
+            if (charge === 4) {
+                setCharge(1)
+            }
+
+        }, 1000);
+    }
+
+    useEffect(() => {
+        charging();
+    }, [charge])
+
+    return (
+        <div className="battery">
+            {[1, 2, 3, 4].map((item, i) => {
+                return (<div key={i} className={charge > i ? "charging cell" : "cell"}></div>
+                )
+            })}
+
+            {/* <div className={charge > 0 ? "charging cell" : "cell"}></div>
+            <div className={charge > 1 ? "charging cell" : "cell"}></div>
+            <div className={charge > 2 ? "charging cell" : "cell"}></div>
+            <div className={charge > 3 ? "charging cell" : "cell"}></div> */}
+        </div>
+    )
+}
+
+export default Battery
