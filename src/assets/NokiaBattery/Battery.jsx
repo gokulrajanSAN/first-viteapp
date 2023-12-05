3509ac9bc9c72208eaa9ed427a319dff6e7f228c
Gokul-HP <gvarutharaj@gmail.com>
2023-12-05 21:58:47 +0530

    fg

--- src/assets/NokiaBattery/Battery.jsx
+++ src/assets/NokiaBattery/Battery.jsx
@@ -2,7 +2,13 @@ import { useContext, useEffect, useState } from "react"
 import "./Battery.css"
 import { TimeContext } from "../Context/Context"
 
-const Battery = () => {    
+// import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+
+const Battery = () => {
 
     const [charge, setCharge] = useState(0)
 
@@ -12,33 +18,54 @@ const Battery = () => {
         setTimeout(() => {
             if (charge <= 3) {
                 setCharge(charge + 1)
-            }   
+            }
             if (charge === 4) {
                 setCharge(1)
             }
-        }, time*1000);
+        }, time * 1000);
     }
 
     useEffect(() => {
         charging();
 
         console.log(time);
-    
-        return ()=>{clearTimeout(charging)}
+
+        return () => { clearTimeout(charging) }
     }, [charge])
 
-    return (
-        <div className="battery">
-            {[1, 2, 3, 4].map((item, i) => {
-                return (<div key={i} className={charge > i ? "charging cell" : "cell"}></div>
-                )
-            })}
+    const Item = styled(Paper)(({ theme }) => ({
+        // backgroundColor: "red",
+        // theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        height:'4rem',
+        color: theme.palette.text.secondary,
+    }));
 
-            {/* <div className={charge > 0 ? "charging cell" : "cell"}></div>
-            <div className={charge > 1 ? "charging cell" : "cell"}></div>
-            <div className={charge > 2 ? "charging cell" : "cell"}></div>
-            <div className={charge > 3 ? "charging cell" : "cell"}></div> */}
-        </div>
+    return (
+
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={0}>
+                {[1, 2, 3, 4].map((item, i) => {
+                    return (<Grid xs={2} key={i}>
+                        <Item sx={{ backgroundColor: charge > i ? "red" : "white" }}></Item>
+                    </Grid>)
+                })}
+            </Grid>
+        </Box >
+
+        // <div className="battery">
+        //     {[1, 2, 3, 4].map((item, i) => {
+        //         return (<div key={i} className={charge > i ? "charging cell" : "cell"}></div>
+        //         )
+        //     })}
+
+        //     {/* <div className={charge > 0 ? "charging cell" : "cell"}></div>
+        //     <div className={charge > 1 ? "charging cell" : "cell"}></div>
+        //     <div className={charge > 2 ? "charging cell" : "cell"}></div>
+        //     <div className={charge > 3 ? "charging cell" : "cell"}></div> */}
+        // </div>
     )
 }
 
